8fcc1020d4ed3e344041a4ea0bcdb413f8815b05
tarekahmed <tarekahmedb200@gmail.com>
2024-06-11 14:57:55 +0300

    Initial Commit

---
+++ MyReceipes/MyReceipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReceipesApp.swift
+//  MyReceipes
+//
+//  Created by tarek ahmed on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyReceipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
